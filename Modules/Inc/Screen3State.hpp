e75085833c9877487c6dadbb11f46dbdd0c44efe
Kacper Sudul <kacper.sudul@gmail.com>
2025-04-06 14:59:08 +0200

    Fix EQ sliders state preserving
    Equalizer screen would not remember negative values
    due to Screen3State class having unsigned variables.
    
    Signed-off-by: Kacper Sudul <kacper.sudul@gmail.com>

--- Modules/Inc/Screen3State.hpp
+++ Modules/Inc/Screen3State.hpp
@@ -8,22 +8,22 @@ class Screen3State {
 public:
     static Screen3State& getInstance();
 
-    uint8_t getLowSliderValue() const;
-    uint8_t getMidSliderValue() const;
-    uint8_t getHighSliderValue() const;
+    int8_t getLowSliderValue() const;
+    int8_t getMidSliderValue() const;
+    int8_t getHighSliderValue() const;
 
-    void setLowSliderValue(uint8_t value);
-    void setMidSliderValue(uint8_t value);
-    void setHighSliderValue(uint8_t value);
+    void setLowSliderValue(int8_t value);
+    void setMidSliderValue(int8_t value);
+    void setHighSliderValue(int8_t value);
 
 private:
     Screen3State();
     Screen3State(const Screen3State&) = delete;
     Screen3State& operator=(const Screen3State&) = delete;
 
-    uint8_t lowSliderValue;
-    uint8_t midSliderValue;
-    uint8_t highSliderValue;
+    int8_t lowSliderValue;
+    int8_t midSliderValue;
+    int8_t highSliderValue;
 };
 
 #endif /* __SCREEN3STATE_HPP_ */
--- Modules/Src/Screen3State.cpp
+++ Modules/Src/Screen3State.cpp
@@ -7,10 +7,10 @@ Screen3State& Screen3State::getInstance() {
 
 Screen3State::Screen3State() : lowSliderValue(0), midSliderValue(0), highSliderValue(0) {}
 
-uint8_t Screen3State::getLowSliderValue() const { return lowSliderValue; }
-uint8_t Screen3State::getMidSliderValue() const { return midSliderValue; }
-uint8_t Screen3State::getHighSliderValue() const { return highSliderValue; }
+int8_t Screen3State::getLowSliderValue() const { return lowSliderValue; }
+int8_t Screen3State::getMidSliderValue() const { return midSliderValue; }
+int8_t Screen3State::getHighSliderValue() const { return highSliderValue; }
 
-void Screen3State::setLowSliderValue(uint8_t value) { lowSliderValue = value; }
-void Screen3State::setMidSliderValue(uint8_t value) { midSliderValue = value; }
-void Screen3State::setHighSliderValue(uint8_t value) { highSliderValue = value; }
+void Screen3State::setLowSliderValue(int8_t value) { lowSliderValue = value; }
+void Screen3State::setMidSliderValue(int8_t value) { midSliderValue = value; }
+void Screen3State::setHighSliderValue(int8_t value) { highSliderValue = value; }
